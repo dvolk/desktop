3c2a8f998b41620d8b379344487edb4e6c8ed1fc
Denis Volk <denis.volk@gmail.com>
2023-01-23 22:49:01 +0000

    speed up scrolling in chromium with imwheel

--- roles/desktop/files/start.sh
+++ roles/desktop/files/start.sh
@@ -14,6 +14,9 @@ xmodmap -e "keycode 66 = Return"
 xinput set-prop "pointer:ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse" 'libinput Button Scrolling Button' 11
 xinput set-prop "pointer:ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse" 'libinput Scroll Method Enabled' 0 0 1
 
+# speed up scrolling
+imwheel -b "4 5"
+
 FILE=start-local.sh
 if [[ -f "$FILE" ]]; then
     bash ./start-local.sh
