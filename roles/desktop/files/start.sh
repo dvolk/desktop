395fd7c8f8a17d019b2e02387d41d28b2cb6c756
Denis Volk <denis.volk@gmail.com>
2023-06-23 23:48:17 +0000

    stuff

--- roles/desktop/files/start.sh
+++ roles/desktop/files/start.sh
@@ -8,9 +8,6 @@ xset s 3600
 
 xsetroot -mod 3 3 -fg black -bg lightgreen
 
-xmodmap -e "clear Lock"
-xmodmap -e "keycode 66 = Return"
-
 xinput set-prop "pointer:ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse" 'libinput Button Scrolling Button' 11
 xinput set-prop "pointer:ELECOM TrackBall Mouse DEFT Pro TrackBall Mouse" 'libinput Scroll Method Enabled' 0 0 1
 
@@ -20,4 +17,9 @@ imwheel -b "4 5"
 FILE=start-local.sh
 if [[ -f "$FILE" ]]; then
     bash ./start-local.sh
+fi
+
+FILE=key.sh
+if [[ -f "$FILE" ]]; then
+    bash ./key.sh
 fi
--- roles/kitty/files/ssh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-d=${1#ssh://}
-kitty ssh $d &
