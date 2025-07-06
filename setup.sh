e18f283763f73f87ad2b4066346ff7c00f67dfa9
Denis Volk <denis.volk@gmail.com>
2025-07-06 09:27:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -624,7 +624,9 @@ cat <<EOF > /tmp/policies.json
              "privacy.globalprivacycontrol.enabled": { "Value": true, "Status": "locked" },
              "privacy.donottrackheader.enabled": { "Value": true, "Status": "locked" },
 
-             "network.dns.native_https_query": { "Value": true, "Status": "locked" }
+             "network.dns.native_https_query": { "Value": true, "Status": "locked" },
+
+             "widget.non-native-theme.scrollbar.size.override": { "Value": 30, "Status": "locked" }
          },
          "Bookmarks": [
              {"Title": "Archwiki: Window manager", "URL": "https://wiki.archlinux.org/title/Window_manager", "Placement": "menu", "Folder": "X11 Window managers/desktops"},
